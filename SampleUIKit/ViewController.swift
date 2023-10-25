5f1481416f3784979daeb4c8d83d3dc0c82aef9d
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-10-25 18:38:05 +0900

    Initial Commit

---
+++ SampleUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleUIKit
+//
+//  Created by Anastasia Grey on 2023/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
